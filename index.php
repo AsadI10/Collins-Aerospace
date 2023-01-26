2658dd3cf7bce76c98c4d7427c4dfec70d2fe33a
FinnMx <finnmoorhouse@gmail.com>
2023-01-26 00:32:03 +0000

    Update index.php

--- index.php
+++ index.php
@@ -12,15 +12,20 @@
  Header fields:
 
  https://reqbin.com/Article/HttpPost
+
+ also refer to this to demonstrate
+ CURL_OPT functions:
+ https://stackoverflow.com/questions/61266770/how-to-get-oauth-2-0-using-php-curl-with-client-credentials-as-grant-type
  */
 
 $ch = curl_init();
 
 
 curl_setopt($ch, CURLOPT_URL, "https://hallam.sci-toolset.com/api/v1/token"); //set API URL
-curl_setopt($ch, CURLOPT_POST, TRUE); //enable POST requests
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); //enables returned JSON from execution
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); //disables SSL/TPL for execution
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); // **
 
 //setting header variables for API call. (So API reaches correct Endpoint)
 $headers = array(
@@ -32,18 +37,17 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); //provides the CURLOPT_HTTPHEADE
 
 //same as header except POST
 $data = array(
-    'grant_type' => 'password&username=hallam&password=9JS(g8Zh'
+    'grant_type=password&username=hallam&password=9JS(g8Zh'
 );
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data); //same as header except POST
 
 
-//set the <USERNAME>:<PASSWORD> for the API's communication with us... the client.
-$userdetail = "123456789:122222137438275285"; //gibberish username and pass
+//set the <CLIENTID>:<CLIENTSECRET> for the API's communication with us... the client.
+$userdetail = "hallam:9JS(g8Zh"; //gibberish username and pass
 curl_setopt($ch,CURLOPT_USERPWD, $userdetail); //same as setting an option for the header except its for <USERNAME>:<PASSWORD> and takes a string
 
 //executes the curl request and gets the status code (200) being success
 $result = curl_exec($ch);  
-$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
 //prints log to screen
 print($result);
