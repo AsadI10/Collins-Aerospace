d4d72b3dbbc7017aee8ceea6ac45e38c805c192a
FinnMx <finnmoorhouse@gmail.com>
2023-01-27 14:35:32 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,32 @@
 <!DOCTYPE html>
 <html>
      <head>
-               <meta charset="UTF-8">
-               <meta name="viewport" content="width=device-width, initial-scale=1.0">
-               <title>Collins Team One</title>
-               <!--Leaflet-->
-               <link rel="stylesheet" href="lib/map/leaflet.css">
-               <script src="lib/map/leaflet.js"></script>
-               <style type="text/css">
-                    #map{ height: 400px;}
-               </style>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Collins Team One</title>
+          <!--Leaflet-->
+          <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+          integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
+          crossorigin=""/>
+          <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
+          integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM="
+          crossorigin=""></script>
+          <style type="text/css">
+               #map {
+                    overflow: auto;
+                    height: 1080px;
+               }
+          </style>
      </head>
 
      <body>
           <div id="map"></div>
           <script>
                var map = L.map('map').setView([51.505, -0.09], 13);
+               L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+               maxZoom: 19,
+               attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+               }).addTo(map);
           </script>
      </body>
 
