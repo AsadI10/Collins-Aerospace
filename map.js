1217c3ddc96c2bf849bd98777ec58c642e602ab0
FinnMx <finnmoorhouse@gmail.com>
2023-02-03 23:36:17 +0000

    Update map.js
    markers now have all GeoJSON meta data attatched.

--- map.js
+++ map.js
@@ -1,9 +1,13 @@
-//----------------------------------------------------------
-//COULD POTENTIALLY WRAP ALL THIS CODE IN THE FETCH DATA
-//SECTION SO THAT WE CAN USE THE DATA THROUGHOUT THE PROGRAM.
-//----------------------------------------------------------
-
 //ALL MAP DATA AND ASSOCIATED FUNCTIONS.
+fetch('Fetch_product_data.php')
+.then(function(response){
+     return response.json();
+}).then(function(data){
+
+//---------
+//CODE BODY
+//---------
+
 var userpoints = [];
 var markers = [];
 var map = L.map('map').setView([53.45043, -2.25975], 13);
@@ -15,45 +19,38 @@ maxZoom: 18,
 attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+
 //Creates a marker for each product pulled from the API
 //also applies event based functions to each marker +
 //attributes.
-function loadProducts(){
-     fetch('Fetch_product_data.php')
-     .then(function(response){
-          return response.json();
-     }).then(function(data){
-          for(let i = 0; i < 121; i++){
-               var tmp = JSON.parse(data[i]);
-               var id = tmp.product.id;
-               var centre = tmp.product.result.centre;
-               var latlang = centre.split(',');
-               markers[i] = L.marker([latlang[0],latlang[1]],{
-                    title: id,
-                    GeoJSON: JSON.parse(data[i])
-               }).addTo(map).bindPopup(id);
-               markers[i].on('click',onClick_Marker);
-          }
-          
-     });
+for(let i = 0; i < 121; i++){
+     var tmp = JSON.parse(data[i]);
+     var id = tmp.product.id;
+     var centre = tmp.product.result.centre;
+     var latlang = centre.split(',');
+     L.marker([latlang[0],latlang[1]],{
+          title: id,
+          GeoJSON: tmp
+     }).addTo(map).bindPopup(id).on('click',onClick_Marker);
 }
 
-//map.on('contextmenu', oncontextmenu);
-//map.on('click', onMapClick);
-loadProducts();
-
-
+/*
 function onMapClick(e) {
      userpoints.push(e.latlng);
      L.marker(e.latlng).addTo(map);
-}
+}    
 
 function oncontextmenu(e) {
      var polygon;
      polygon = L.polygon(points).addTo(map);
      userpoints = [];
 }
+*/
 
-//todo
+//when a marker is clicked all of its metadata is returned
 function onClick_Marker(e){
+     var gj = e.sourceTarget.options.GeoJSON;
+     console.log(gj);
 }
+
+});
