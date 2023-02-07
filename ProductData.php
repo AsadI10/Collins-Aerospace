a8cdfaa8d2958ba10fcd4ce5683a8a4956794a97
Burdett <c0031318@hallam.shu.ac.uk>
2023-02-07 10:43:07 +0000

    magic hands fixing the stuff

---
+++ ProductData.php
@@ -0,0 +1,21 @@
+<?php
+	class ProductData
+	{
+		private $_Identifier;
+
+		function __construct($identifier){
+			$this->_Identifier;
+		}
+		
+		public function GetIdentifer(){
+			return $this->_Identifier; 
+		}
+
+		public static function LoadFromCache($identifier){
+
+		}
+		public function SaveToCache(){
+
+		}
+	}
+?>
