6d6661192dc6a6156c2025da06a4593bca59ce3b
James21190 <James211902@gmail.com>
2023-02-07 14:59:05 +0000

    - Commented ProductData.php

--- ProductData.php
+++ ProductData.php
@@ -1,29 +1,42 @@
 <?php
+	// Path to the SQLite database for caching.
 	const SQL_FILE = "./cache.sql";
 
 	class ProductData
 	{
+		// ======
+		// Fields
+		// ======
 		private $_Identifier;
+
 		public $Name;
+		// Coordinates of the center of this product
 		public $Center;
+		// Date in ms from
 		public $DateCreated;
+
+		// =========
+		// Functions
+		// =========
 
 		function __construct($identifier){
 			$this->_Identifier;
 		}
 		
+		// Returns the identifier of this product.
 		public function GetIdentifer(){
 			return $this->_Identifier;
 		}
 
+		// Fetches a ProductData object that has been cached in the SQLite database.
 		public static function LoadFromCache($identifier){
-
+			$handle = new SQLite3(SQL_FILE);
 		}
 
+		// Saves this object into the SQLite database.
+		// Updates an existing record or creates a new record if one doesn't exist already.
 		public function SaveToCache(){
 			$handle = new SQLite3(SQL_FILE);
-
-			$sql = "UPDATE ProductData"
 		}
 	}
 ?>
