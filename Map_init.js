00e9aadd6afe6d609407302b6cdbb50d8681840e
FinnMx <93927783+FinnMx@users.noreply.github.com>
2023-02-13 11:21:40 +0000

    reverting

--- Map_init.js
+++ Map_init.js
@@ -1,66 +1,10 @@
-//ALL MAP DATA AND ASSOCIATED FUNCTIONS.
-// This is a debug thing and doesn't do anything important. aiggghhhttttt
+var userpoints = [];
+var map = L.map('map').setView([53.45043, -2.25975], 13);
+var markers = new L.LayerGroup().addTo(map);
 
-fetch('Fetch_product_data.php')
-    .then(function (response) {
-        console.log(response.text());
-});
+document.getElementsByClassName('leaflet-control-attribution')[0].style.display = 'none';
 
-fetch('Fetch_product_data.php')
-    .then(function (response) {
-        // TODO: Error checking to check if response is json or not
-        return response.json();
-    }).then(function (data) {
-        //---------------
-        //---CODE BODY---
-        //---------------
-        //Creates a marker for each product pulled from the API
-        //also applies event based functions to each marker +
-        //attributes.
-        let map = new Map();
-
-        for (let i = 0; i < data.length; i++) {
-            var tmp = data[i];
-            var id = tmp["_Identifier"];
-            var centre = tmp["_Centre"];
-            var latlang = centre.split(',');
-            L.marker([latlang[0], latlang[1]], {
-                title: id,
-                GeoJSON: tmp
-            }).addTo(markers).bindPopup(id).on('click', onClick_Marker);
-        }
-
-        map.on('contextmenu', oncontextmenu);
-        map.on('click', onMapClick);
-        /*
-        =====================
-        POLYGON DRAWING LOGIC
-        =====================
-
-        function onMapClick(e) {
-            userpoints.push(e.latlng);
-            L.marker(e.latlng).addTo(map);
-        }
-
-        function oncontextmenu(e) {
-            var polygon;
-            var body;
-            polygon = L.polygon(userpoints).addTo(map);
-            markers.getLayers().forEach(element => {
-                if (polygon.contains(element._latlng)) {
-                    body = body + "<br>" + element.options.title;
-                }
-            });
-            //displays all the products within the polygon onto the panel
-            document.getElementById('panel1').innerHTML = body;
-            userpoints = [];
-        }
-        */
-
-        //when a marker is clicked all of its metadata is returned
-        function onClick_Marker(e) {
-            var gj = e.sourceTarget.options.GeoJSON;
-            document.getElementById('panel1').innerHTML = JSON.stringify(gj);
-        }
-
-    });
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
--- map.js
+++ map.js
@@ -1,17 +1,65 @@
-class Map{
+//ALL MAP DATA AND ASSOCIATED FUNCTIONS.
+// This is a debug thing and doesn't do anything important. aiggghhhttttt
+/*
+fetch('Fetch_product_data.php')
+    .then(function (response) {
+        console.log(response.text());
+});
+*/
+fetch('Fetch_product_data.php')
+    .then(function (response) {
+        // TODO: Error checking to check if response is json or not
+        return response.json();
+    }).then(function (data) {
 
-    constructor(){
-        //var userpoints = [];
-        var map = L.map('map').setView([53.45043, -2.25975], 13);
-        var markers = new L.LayerGroup().addTo(map);
-        document.getElementsByClassName('leaflet-control-attribution')[0].style.display = 'none';
+        console.log(data);
 
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 18,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
-    }
+        //---------------
+        //---CODE BODY---
+        //---------------
 
-}
+        //Creates a marker for each product pulled from the API
+        //also applies event based functions to each marker +
+        //attributes.
+        for (let i = 0; i < data.length; i++) {
+            var tmp = data[i];
+            var id = tmp["_Identifier"];
+            var centre = tmp["_Centre"];
+            var latlang = centre.split(',');
+            L.marker([latlang[0], latlang[1]], {
+                title: id,
+                GeoJSON: tmp,
+            }).addTo(markers).bindPopup(id).on('click', onClick_Marker);
+        }
 
-module.exports = Map;
+        map.on('contextmenu', oncontextmenu);
+        map.on('click', onMapClick);
+
+        function onMapClick(e) {
+            userpoints.push(e.latlng);
+            L.marker(e.latlng).addTo(map);
+        }
+
+        function oncontextmenu(e) {
+            var polygon;
+            var body;
+            polygon = L.polygon(userpoints).addTo(map);
+            markers.getLayers().forEach(element => {
+                if (polygon.contains(element._latlng)) {
+                    body = body + "<br>" + element.options.title;
+                }
+            });
+            //displays all the products within the polygon onto the panel
+            document.getElementById('panel1').innerHTML = body;
+            userpoints = [];
+        }
+
+
+        //when a marker is clicked all of its metadata is returned
+        function onClick_Marker(e) {
+            var gj = e.sourceTarget.options.GeoJSON;
+            var body = "ID: " + gj.product.result.identifier + "<br>NAME: " + gj.product.result.title + "<br><br>COORDINATES: " + gj.product.result.centre;
+            document.getElementById('panel1').innerHTML = body;
+        }
+
+    });
