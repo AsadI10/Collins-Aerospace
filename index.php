a1bdc03037e2be75017b11eefec760f0fababebb
Asadi <imtiazasad74@gmail.com>
2023-02-13 18:05:48 +0000

    INDEX page

--- index.php
+++ index.php
@@ -66,7 +66,33 @@
 <br>
 <!-- span used to create a chart on the right hand side  -->
      <span id="panel1" class="d-block p-2 bg-dark text-white">
-          
+     <script type="text/javascript">
+      google.charts.load("current", {packages:["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+         ['Task', 'Hours per Day'],
+          ['Plane', 11],
+          ['Jet', 2],
+          ['Military', 2],
+          ['Random', 2],
+          ['On ground', 7]
+        ]);
+
+        var options = {
+          backgroundColor: 'transparent',
+          is3D: true,
+          'width':400,
+          'height':300,
+          'title' : 'My Chart'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+        chart.draw(data, options);
+      }
+    </script>
+    <div id="piechart" style="width: 350px; height: 200px;"></div>
+    
      </span>
 
      <div id="map">
