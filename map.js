a7ca5fa32fe664bf94e5b878cee8d6dc99377934
James21190 <James211902@gmail.com>
2023-02-14 14:31:17 +0000

    - Removed a thing

--- map.js
+++ map.js
@@ -16,16 +16,6 @@ function onShiftDrag(e){
 
     var rectangle = L.rectangle(bounds).addTo(shapes);
 
-    const getMethods = (obj) => {
-        let properties = new Set()
-        let currentObj = obj
-        do {
-            Object.getOwnPropertyNames(currentObj).map(item => properties.add(item))
-        } while ((currentObj = Object.getPrototypeOf(currentObj)))
-        return [...properties.keys()].filter(item => typeof obj[item] === 'function')
-    }
-    console.log(getMethods(rectangle));
-
     let arr = [];
     //can be put into its own function
     markers.getLayers().forEach(element=>{
