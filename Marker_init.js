e3b25e05842ab46f894835c315b598349e093707
FinnMx <93927783+FinnMx@users.noreply.github.com>
2023-02-14 10:47:58 +0000

    - Adding drawing footprints

--- Marker_init.js
+++ Marker_init.js
@@ -7,10 +7,13 @@ function loadMarkers(pulledData){
         var tmp = pulledData[i];
         var id = tmp["Identifier"];
         var centre = tmp["Centre"];
+        var footp = tmp["Footprint"];
         var latlang = centre.split(',');
+
         L.marker([latlang[0], latlang[1]], {
             title: id,
             GeoJSON: tmp,
+            footprint: footp
         }).addTo(markers).bindPopup(id)
             .on('click', onClick_Marker)
             .on('mouseover',onMouseOver_marker);
