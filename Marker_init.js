55b6bf76b314df389aef31961e79add4a0024895
FinnMx <93927783+FinnMx@users.noreply.github.com>
2023-02-14 11:35:01 +0000

    Update Marker_init.js

--- Marker_init.js
+++ Marker_init.js
@@ -2,21 +2,25 @@
 // Initilizes all markers on the map
 //===================================
 
+// Loads an array of ProductData and displays them as markers on the map
 function loadMarkers(pulledData){
     for (let i = 0; i < pulledData.length; i++) {
         var tmp = pulledData[i];
         var id = tmp["Identifier"];
+        // Parse the coordinates of the centre
         var centre = tmp["Centre"];
         var footp = tmp["Footprint"];
         var latlang = centre.split(',');
 
-        
-
         L.marker([latlang[0], latlang[1]], {
+            icon: greenIcon,
+            // Set tags
             title: id,
             GeoJSON: tmp,
             footprint: footp
+        // Add to the maps collection of markers
         }).addTo(markers).bindPopup(id)
+            // Set events
             .on('click', onClick_Marker)
             .on('mouseover',onMouseOver_marker);
     }
