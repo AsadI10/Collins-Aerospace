afd6a2d2ffaa82bd98ad962361d7925201285981
Asadi <imtiazasad74@gmail.com>
2023-02-14 10:15:37 +0000

    css and map

--- Map_init.js
+++ Map_init.js
@@ -9,8 +9,8 @@ L.control.scale().addTo(map);
 
 document.getElementsByClassName('leaflet-control-attribution')[0].style.display = 'none';
 
+//Used for search control
 var searchControl = new L.esri.Controls.Geosearch().addTo(map);
-
 var results = new L.LayerGroup().addTo(map);
 searchControl.on('results', function(data){
 results.clearLayers();
@@ -19,10 +19,10 @@ results.addLayer(L.marker(data.results[i].latlng));
 }
 });
 
-// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     maxZoom: 18,
-//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-// }).addTo(map);
-var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+// var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+// 	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+// }).addTo(map);
