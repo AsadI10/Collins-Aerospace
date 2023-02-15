ff8e6c28b4bf7ebd6856ff170f30b45bd0e42fdd
James21190 <James211902@gmail.com>
2023-02-15 11:03:56 +0000

    stuff

--- ErrorHandler.php
+++ ErrorHandler.php
@@ -3,20 +3,18 @@
 	function RaiseError($sender, $message){
 		header("Location: ./ErrorHandler.php?ErrorSender='".$sender."'&ErrorMessage='".$message."'");
 	}
-
-	if(isset($_GET["ErrorSender"]) && isset($_GET["ErrorMessage"])){
-		?>
-		<h1>Fatal Error Raised</h1>
-		Sender: <?php echo $_GET["ErrorSender"]; ?><br>
-		Message: <?php echo $_GET["ErrorMessage"]; ?><br>
-		<a href="index.php">Return to home page</a>
-		<?php
-	}
-	else{
-		if(basename($_SERVER['PHP_SELF']) == "ErrorHandler.php"){
+	if(basename($_SERVER['PHP_SELF']) == "ErrorHandler.php"){
+		if(isset($_GET["ErrorSender"]) && isset($_GET["ErrorMessage"])){
+			?>
+			<h1>Fatal Error Raised</h1>
+			Sender: <?php echo $_GET["ErrorSender"]; ?><br>
+			Message: <?php echo $_GET["ErrorMessage"]; ?><br>
+			<a href="index.php">Return to home page</a>
+			<?php
+		}
+		else{
 			header("Location: ./index.php");
 		}
-
 	}
 
 ?>
--- index.php
+++ index.php
@@ -47,32 +47,6 @@
           <script src="Sidebar.js"></script>
           <script src="./lib/map/wise-leaflet-pip.js"></script>
      </head>
-    <head>
-        <title>Collins Team One</title>
-          
-        <!--Leaflet-->
-        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
-        integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
-        crossorigin=""/>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-        <link rel="stylesheet" href="CSS/index.css"/>
-        <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
-        integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM="
-        crossorigin=""></script>
-        <script src="/lib/map/wise-leaflet-pip.js" type="text/javascript"></script>
-        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-        <!-- use this to get the search bar -->
-        <script src="https://cdn-geoweb.s3.amazonaws.com/esri-leaflet/0.0.1-beta.5/esri-leaflet.js"></script>
-        <script src="https://cdn-geoweb.s3.amazonaws.com/esri-leaflet-geocoder/0.0.1-beta.5/esri-leaflet-geocoder.js"></script>
-        <link rel="stylesheet" type="text/css" href="https://cdn-geoweb.s3.amazonaws.com/esri-leaflet-geocoder/0.0.1-beta.5/esri-leaflet-geocoder.css">
-        <script src="./lib/map/wise-leaflet-pip.js"></script>
-          
-        <!-- Our Stuff -->
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <script src="GetPage.js"></script>
-        <script src="Sidebar.js"></script>
-    </head>
 
     <body>
         <!-- Header -->
