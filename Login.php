ae39fb884f9249e57172d25c0c41e92663370011
Ikhan101 <c1008166@hallam.shu.ac.uk>
2023-02-15 17:55:43 +0000

    Login

--- Login.php
+++ Login.php
@@ -1,5 +1,35 @@
-<form action="index.php" method="POST">
-	<input name="Username" type="text" value="hallam">
-	<input name="Password" type="password" value="9JS(g8Zh">
-	<input type="submit" value="Login">
-</form>
+<h1>Login </h1>
+
+<div class="logindiv">
+	<fieldset action="index.php" method="POST">
+		<legend>Login</legend>
+		<label class="loginlabel" for="">Username: </label>
+		<input name="Username" type="text" value="hallam">
+		<label class="loginlabel" for="">Password:</label>
+		<input name="Password" type="password" value="9JS(g8Zh">
+		<input type="submit" value="Login">
+</fieldset>
+</div>
+
+<style>
+	body{
+		background: #2d545e;
+	}
+	h1{
+		font-weight: bold;
+		margin: 0 auto;
+		width: 8%;
+	}
+	div {
+		border: solid 3px grey;
+		background: ;
+        width: 50%; /* Set the width of the div */
+        margin: 0 auto; /* Center the div horizontally */
+    }
+	/* .loginlabel{
+		font-size: 19px;
+		color: white;
+		font-weight: bold;
+	} */
+ 
+</style>
