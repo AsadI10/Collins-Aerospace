10ac01d975304ea0df7f303a9a04013f3367f3d6
Ikhan101 <c1008166@hallam.shu.ac.uk>
2023-02-16 13:18:33 +0000

    sidebarindex

--- sidebarheader.php
+++ sidebarheader.php
@@ -5,12 +5,12 @@
         }
     </script>
 <br>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
-  </button>
+    </button>
   <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-    <a class="CompanyName" href="#">Collins Aerospace</a>
+    <a class="CompanyName" href="#">Aerospace Discovery</a>
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
       <li class="nav-item active">
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
@@ -19,7 +19,7 @@
         <a class="nav-link" href="#">Link</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
+        <a class="nav-link disabled" onclick="Logout()">Logout</a>
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
@@ -32,8 +32,12 @@
 
 <style>
     .CompanyName{
-        font-size: 19px;
+        font-size: 25px;
         font-weight: bold;
         color: black;
+    }
+    .collapse navbar-collapse ul li{
+        font-size: 20px;
+
     }
 </style>
