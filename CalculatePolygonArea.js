7693909e33f66d128b1e7084463038f2858d2b38
James21190 <James211902@gmail.com>
2023-02-16 13:49:53 +0000

    - CalculatePolygonArea is completed (untested)

--- CalculatePolygonArea.js
+++ CalculatePolygonArea.js
@@ -1,6 +1,21 @@
+//from https://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula
+function getDistance(lat1, lon1, lat2, lon2) {
+    var p = 0.017453292519943295;    // Math.PI / 180
+    var c = Math.cos;
+    var a = 0.5 - c((lat2 - lat1) * p) / 2 +
+        c(lat1 * p) * c(lat2 * p) *
+        (1 - c((lon2 - lon1) * p)) / 2;
+
+    return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+}
+
 function CalculateTriArea(x, y, z) {
-    console.log(x, y, z);
-    return 1;
+    // Heron's formula
+    var a = getDistance(x[0], x[1], y[0], y[1]);
+    var b = getDistance(y[0], y[1], z[0], z[1]);
+    var c = getDistance(x[0], x[1], z[0], z[1]);
+    var s = (a + b + c) / 2;
+    return Math.sqrt(s * (s - a) * (s - b) * (s - c));
 }
 
 function CalculatePolygonArea(coordinateArr) {
