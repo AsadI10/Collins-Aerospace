040b41b65f2748c3adf5da74845f97f128f5f32e
Ikhan101 <c1008166@hallam.shu.ac.uk>
2023-02-17 09:48:34 +0000

    charts

--- Charts.js
+++ Charts.js
@@ -19,7 +19,8 @@ function loadPieChart(data, baseval = 248532){
           backgroundColor: 'transparent',
           chartArea: { height: '500', left: 0, right: 0, top: 10, bottom: 0 },
           legend: { position: 'none'},
-          colors: ['#333333', '#242424']
+          // colors: ['#333333', '#242424']
+          colors: ['#bac9b9', '#102de8']
         };
 
         var chart = new google.visualization.PieChart(document.getElementById('pieChart'));
