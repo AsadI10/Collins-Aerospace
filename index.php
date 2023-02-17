1ca42a2419230b3febba131ccec477350e078028
Asadi <imtiazasad74@gmail.com>
2023-02-17 11:28:43 +0000

    css

--- index.php
+++ index.php
@@ -77,6 +77,7 @@
           <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
           <script src="Charts.js"></script>
           <script src="CalculatePolygonArea.js"></script>
+          <script src="index.js"></script>
 
 </head>
 <body>
