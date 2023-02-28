66db62b627b827bf7ce375053787a27cfe1007e9
Ikhan101 <c1008166@hallam.shu.ac.uk>
2023-02-28 10:15:52 +0000

    helppage

--- Help.php
+++ Help.php
@@ -86,8 +86,12 @@
 
 <ul>
   <li>Login</li>
+  <li>interactable nav bar with some options</li>
   <li>Press on the stalite icon</li>
   <li>press shift for polygon</li>
+  <li>Zoom in and out of the map</li>
+  <li>use the side panal for the information</li>
+  <li>pie chart for the coverage</li>
 </ul>
 
 </body>
