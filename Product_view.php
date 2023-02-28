1ba103f0bdcb8794224f37a6a36224a249396e70
Unknown <James211902@gmail.com>
2023-02-28 10:11:24 +0000

    - Added header to product view page

--- Product_view.php
+++ Product_view.php
@@ -5,6 +5,8 @@ if(!isset($_GET['identifier'])){
 require_once("./APIInterface.php");
 require_once("./SessionMaster.php");
 
+include("PageHeader.php");
+
 $_SESSION['APIInterface']->GetRawData($_GET['identifier']);
 $json_string = json_encode($_SESSION['APIInterface']->GetRawData($_GET['identifier']), JSON_PRETTY_PRINT);
 echo $json_string;
