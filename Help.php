e817b791337fc9736a60ff2cd87fd4ef92ee7486
Ikhan101 <c1008166@hallam.shu.ac.uk>
2023-03-14 09:55:22 +0000

    help

--- Help.php
+++ Help.php
@@ -148,7 +148,7 @@ weeks in order to demonstrate our ability to create a profesional application.
     font-size: 16px; 
   }
   .Map-layers{
-    width:35%;
+    width:100%;
     display:block;
     margin-left:auto;
     margin-right:auto;
