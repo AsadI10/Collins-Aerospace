31967bd67076e760d9d59f4d56c33449e12e856b
Ikhan101 <c1008166@hallam.shu.ac.uk>
2023-03-28 09:59:08 +0100

    indexsidbar

--- index.php
+++ index.php
@@ -87,6 +87,7 @@
       <a id="generalData-Products"></a>
     </div>
     <input id="MissionSearch" class="searchsidebar" type="text" placeholder="Search..." oninput="ReloadMap()">
+    <button class="ClearButton">Clear</button>
     <div id="panel-info">
     </div>
   </span> 
@@ -104,7 +105,13 @@
 </html>
 
 <style>
+  .ClearButton{
+    font-weight: bold;
+    border: solid 2px black;
+  }
   .searchsidebar{
+    border: solid black 2px;
+    font-weight: bold;
     margin-top:12px;
     display: left;
     margin-left: auto;
